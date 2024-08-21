d666b84ef9f65b59753c29ebe6d7993d9249ee82
mubashir12-a <ma2625645@gmail.com>
2024-08-21 12:53:50 +0530

    21-08-2024

---
+++ Modules.py
@@ -0,0 +1,15 @@
+'''
+On terminal With help of pip command we install external modules.
+E.g: pip install pandas
+
+import helps to import modules in our program:
+'''
+
+import math
+
+r = float(input("Enter radius of circle: "))
+
+area = math.pi * (r * r);
+
+print("Area of circle is : ",area)
+
