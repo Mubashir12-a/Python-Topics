e2ba6910eb3062a7315a64d9045f42fba4b37a2c
mubashir12-a <ma2625645@gmail.com>
2024-08-21 12:53:18 +0530

    21-08-2024

---
+++ Hello-World.py
@@ -0,0 +1,19 @@
+# To print output:
+print("Hello World")
+
+
+# Declare a variable:
+a = 20
+b = 30
+
+
+# Addition of two numbers:
+print(a+b);
+
+
+# Take User Input as integer:
+c = int(input("Enter Number: "))
+
+
+# Print sum of 3 variable:
+print(a + b + c)
