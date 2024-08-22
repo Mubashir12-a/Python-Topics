107c1b8043202217b039b58d053699e2dfe82e2b
mubashir12-a <ma2625645@gmail.com>
2024-08-22 21:34:43 +0530

    22-08-2024

---
+++ Type-Casting.py
@@ -0,0 +1,18 @@
+a = 10
+b = 20
+print(a + b)  # 30;
+
+c = "10"
+d = "20"
+print(c + d)  # 1020;
+
+
+# Conversion of data types:
+
+x = int(c)
+y = int(d)
+print(x + y)
+
+x = float(c)
+y = float(d)
+print(x + y)
