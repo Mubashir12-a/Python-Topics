7e4d49d9789692fe8fe85002b719f5b556ba029c
mubashir12-a <ma2625645@gmail.com>
2024-08-23 22:41:08 +0530

    23-08-2024

---
+++ if-else.py
@@ -0,0 +1,48 @@
+#Conditional statement:
+'''
+In python we don't use brackets for loops block.
+In python identation is considered important thing
+because it represents block of code.
+'''
+
+age = int(input("Enter age: "))
+
+if(age >= 18):
+    print("You can drive.")
+else:
+    print("You can't drive")
+    
+    
+    
+    
+temp = int(input("Enter temperature: "))
+
+if(temp <= 0):
+    print("Its cold temperature outside.")
+elif(temp <= 25):
+    print("Its normal temperature outside")
+else:
+    print("Its hot temperature outside.")    
+
+
+
+#Nested if-else:
+
+# GREATEST BETWEEN 3 NUMBERS:
+
+x = 15
+y = 7
+z = 22
+
+if(x > y):
+    if(x > z):
+        print(f"{x} is biggest num")
+    else:
+        print(f"{z} is biggest num")
+else:
+    if(y > z):
+        print(f"{y} is biggest num")
+    else:
+        print(f"{z} is biggest num")
+
+
