4fe38eb36e00b40d5dc43161ebff8ac509428bba
mubashir12-a <ma2625645@gmail.com>
2024-08-24 23:28:33 +0530

    24-08-2024

---
+++ while-loop.py
@@ -0,0 +1,20 @@
+# print numbers 0 - 10:
+
+i = 0;   # initialization
+
+while i <= 10:         # Condition check
+    print(i, end=" ")
+    i = i + 1          # Increament
+    
+print("\n")
+
+
+# take user password and Grant access:
+
+password = "mubashir"
+user_pass = input("Enter Password: ")
+
+while user_pass != password:
+    user_pass = input("Try again: ")
+    
+print("Access Granted")
